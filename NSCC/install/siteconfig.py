62a0172108c0c7adbeb5cd8415c9c157c41cca72
Yi-Cheng Hsiao <46053065+Yi-Cheng0101@users.noreply.github.com>
2021-05-24 04:52:10 +0800

    Update siteconfig.py

--- NSCC/install/siteconfig.py
+++ NSCC/install/siteconfig.py
@@ -1,1 +1,14 @@
+compiler = 'icc'
+mpicompiler = 'mpicc'
+mpilinker = 'mpicc'
 
+xc = '/home/users/industry/isc2020/iscst07/scratch/packages/libxc-4.3.4/'
+include_dirs += [xc + 'include']
+extra_link_args += [xc + 'lib/libxc.a']
+if 'xc' in libraries:
+	        libraries.remove('xc')
+
+
+
+parallel_python_interpreter = False
+extra_link_args += ['/home/users/industry/isc2020/iscst07/scratch/packages/openblas/lib/libopenblas.a']
